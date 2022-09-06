e6ef05c3324439aaeedba49a4e03fbfff8ece0c2
Lucas McLaughlin <lm@Lucass-MacBook-Pro.local>
2022-09-06 20:18:27 +0100

    First commit for space game. Implemeneted basic movement, basic procedural leveland basic key combo system.
    todo next: Add hitboxes for attacks, make key combos actually attacks and then check collisions between attacks and mobs.
    
    I took out most of the code from the tutorial but started out with this tutorial:
    
     http://www.osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-1-of-3

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+    t.title = "Space Bullets Bounty Fighter"
+    t.version = "11.4"
+    t.window.width = 1024
+    t.window.height = 768
+
+    t.console = true
+
+end
